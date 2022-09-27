e03b8ffa80a1ca537da8c1df76a752195c3b058a
Christian Berendt <berendt@osism.tech>
2022-09-27 16:49:34 +0200

    terraform: fix plugin installation
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- terraform/azure/files/main.tf
+++ terraform/azure/files/main.tf
@@ -2,9 +2,19 @@ terraform {
   required_version = ">= 1.2.0"
 
   required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "= 2.2.3"
+    }
+
+    null = {
+      source = "hashicorp/null"
+      version = "= 3.1.1"
+    }
+
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.24.0"
+      version = "= 3.24.0"
     }
   }
 }
